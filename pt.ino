c24082c1d6f7dcd25f62bd9e5e5d9475bd0fa090
aidan friedrichwillis <aidanfriedrichwillis@gmail.com>
2022-02-23 18:40:57 +0000

    init

---
+++ pt.ino
@@ -0,0 +1,464 @@
+#include <QTRSensors.h>
+#include <Zumo32U4LineSensors.h>
+#include "TurnSensor.h"
+#include <Zumo32U4Encoders.h>
+#include <Zumo32U4Buzzer.h>
+#include <Zumo32U4Motors.h>
+
+Zumo32U4Motors motors;
+Zumo32U4LineSensors lineSensors;
+L3G gyro;
+Zumo32U4LCD lcd;
+Zumo32U4ProximitySensors proxSensors;
+Zumo32U4Encoders encode;
+Zumo32U4Buzzer buzzer;
+
+
+
+#define NUM_SENSORS 3
+unsigned int lineSensorValues[NUM_SENSORS];
+
+int inByte = 0;         // incoming serial byte
+int speedr = 0;         //speed of left engine
+int speedl = 0;         //speed of rigth engine
+bool autoMode = true;
+int calibrateData[3];                                   
+unsigned int sensor_values[NUM_SENSORS];                
+boolean atCorner = false;
+boolean man = false;
+boolean atRoom = false;
+char option = ']';
+int mode = 0; 
+int roomsSeached = 0;
+
+
+
+
+
+#define CALIBERATE_SPEED  200
+
+
+
+int32_t getAngle() {
+  
+  return (((int32_t)turnAngle >> 16) * 360) >> 16;
+}
+
+void rotationAl(int angle){
+  delay(500);
+  turnSensorReset();
+  turnSensorUpdate();
+  while(getAngle()!=-angle){
+             turnSensorUpdate();
+             Serial.println(getAngle());
+            motors.setSpeeds(100, -100);
+   } 
+ }
+
+
+
+void calibrateSensors()
+{  
+  
+  delay(50);
+  encode.init();
+  delay(50);
+  proxSensors.initFrontSensor();
+  delay(50);
+  turnSensorSetup();
+  
+  delay(100);
+  
+  lineSensors.initThreeSensors();
+
+  for(uint16_t i = 0; i < 60; i++){
+    if (i > 15 && i <= 45)
+    {
+      motors.setSpeeds(-50, 50);
+    }
+    else
+    {
+      motors.setSpeeds(50, -50);
+    }
+    
+     lineSensors.calibrate();
+     
+     delay(10);
+
+  }
+
+  
+     for (int i = 0; i < NUM_SENSORS; i++){
+      calibrateData[i] = lineSensors.calibratedMaximumOn[i];
+  }
+
+
+  motors.setSpeeds(0, 0);
+}
+
+void setup()
+{
+
+   Serial.begin(9600);
+   Serial1.begin(9600);
+   char n;
+   while(n!='c'){
+    n = Serial1.read();
+   }
+   calibrateSensors();
+  
+      
+}
+
+void checkAtRoom(){
+
+    if(option == 'p'){
+     Serial1.println("s");
+      mode = 3;
+      }
+      
+  
+ }
+ void checkMan(){
+
+    if(option == 'k'){
+      
+      mode = 1;
+      }
+      
+  
+ }
+
+
+void loop(){
+
+lineSensors.read(sensor_values);
+ turnSensorUpdate();
+// option = Serial.read();
+ option = Serial1.read();
+ 
+
+ checkAtRoom();
+ checkMan();
+
+
+if(mode == 0){
+
+    autoFunc();
+}
+
+ else if (mode == 1){
+  
+    manFunc();
+  }
+
+  else if(mode == 2){
+    
+    
+    cornerControl();
+    
+  }
+  else if (mode == 3){
+    
+    searchRoomControl();
+    
+    }
+
+
+  
+
+    
+}
+
+
+
+void autoFunc(){
+
+
+     if(((sensor_values[2] > calibrateData[2]+100) && (sensor_values[0] > calibrateData[0]+100) && (sensor_values[1] > calibrateData[1]))||sensor_values[1] > calibrateData[1]+100){
+      Serial1.println("c");
+     motors.setRightSpeed(0);
+     motors.setLeftSpeed(0);
+    mode = 2;     
+   }
+
+   else if(sensor_values[0] > calibrateData[0]+100){
+
+       motors.setRightSpeed(0);
+     motors.setLeftSpeed(100);
+  }
+   
+  else if(sensor_values[2] > calibrateData[2]+100){
+
+     motors.setRightSpeed(100);
+     motors.setLeftSpeed(0);
+  }
+  
+
+  else{
+
+     speedr= 100;
+   speedl= 100;
+   
+  motors.setRightSpeed(speedr);
+  motors.setLeftSpeed(speedl);
+    }
+ 
+  
+  }
+
+
+
+  boolean checkPeople(){
+      
+    proxSensors.read();
+    uint8_t leftValue = proxSensors.countsFrontWithLeftLeds();
+    uint8_t rightValue = proxSensors.countsFrontWithRightLeds();
+ 
+
+  if(leftValue > 4 || rightValue > 4){
+          return true;
+    }
+    
+    else{
+        
+        return false;
+
+  }
+  
+  }
+
+  void scanRoom(){
+    boolean isPerson = false;
+
+for(uint16_t i = 0; i <120 ; i++){
+    if (i > 30 && i <= 90)
+    {
+      motors.setSpeeds(-100, 100);
+    }
+    else
+    {
+      motors.setSpeeds(100, -100);
+    }
+     delay(10);
+     isPerson = checkPeople();
+  }
+        motors.setSpeeds(0, 0);
+
+
+       delay(100);
+       if(isPerson){
+              buzzer.playFrequency(500,250,10);
+            Serial1.println("+");
+            
+        
+        }
+        
+    
+    
+    }
+
+  void  moveF(){
+     
+      encode.getCountsAndResetRight();
+
+            while(encode.getCountsRight()<400){
+                motors.setLeftSpeed(100);
+                motors.setRightSpeed(100);
+            }
+            
+            motors.setSpeeds(0, 0);
+      
+
+   
+    }
+    void  moveB(){
+     
+      encode.getCountsAndResetRight();
+
+            while(encode.getCountsRight()>-400){
+                motors.setLeftSpeed(-100);
+                motors.setRightSpeed(-100);
+            }
+            
+            motors.setSpeeds(0, 0);
+      
+
+   
+    }
+  
+
+  void turnRight(){
+       encode.getCountsAndResetLeft();
+            while(encode.getCountsLeft()<700){
+                motors.setLeftSpeed(100);
+                motors.setRightSpeed(-100);
+
+            }
+                motors.setSpeeds(0, 0);
+
+    }
+
+    void turnLeft(){
+       encode.getCountsAndResetRight();
+            while(encode.getCountsRight()<610){
+                motors.setLeftSpeed(-100);
+                motors.setRightSpeed(100);
+            }
+            
+            motors.setSpeeds(0, 0);
+      
+      
+      }
+  
+    void turnAround(){
+      
+      encode.getCountsAndResetRight();
+            while(encode.getCountsRight()<1220){
+                motors.setLeftSpeed(-100);
+                motors.setRightSpeed(100);
+            }
+            
+            motors.setSpeeds(0, 0);
+      
+      
+      }
+ 
+
+    
+  
+  void searchRoomControl(){
+      
+     motors.setSpeeds(0, 0);
+
+    doScan();
+    
+          if(option == 'c'){
+             Serial1.println("b");
+             mode = 0;
+          
+         }
+         
+
+  }
+
+void doScan(){
+  
+  
+  if(option == 'r'){
+           
+            turnRight();
+            delay(500);
+            moveF();
+            delay(500);
+            scanRoom();
+            delay(500);
+            moveB();
+            delay(500);
+            turnLeft();
+
+        }
+        else if(option == 'l'){
+          
+            turnLeft();         
+             delay(500);
+            moveF();
+            delay(500);
+            scanRoom();
+            delay(500);
+            moveB();
+            delay(500);
+            turnRight();
+            
+
+         }
+  
+  
+  
+  
+  }
+
+void cornerControl(){
+
+        if(option == 'r'){
+                      
+          turnRight();
+          
+        }
+        if(option == 'l'){
+                      
+          turnLeft();
+          
+        }
+        else if(option == 'z'){
+
+           turnAround();
+          
+         }
+        else if(option == 'c'){
+            Serial1.println("b");
+             mode = 0;
+      
+         }  
+  
+  
+  }
+
+
+
+
+void manFunc(){
+  //This will speed down the engine unless a command to speed up is received
+  if(abs(speedr)>0){speedr=speedr-speedr/5;}
+  if(abs(speedl)>0){speedl=speedl-speedl/5;}
+  motors.setRightSpeed(speedr);
+  motors.setLeftSpeed(speedl);
+  
+//  if (Serial1.available() > 0){
+    switch(option){
+      case 'a':// turn left
+        speedr=speedr+400;
+        break;
+     case 'd': // turn right
+        speedl=speedl+400;
+        break;
+     case 'w': // forward
+        speedr=speedr+400;
+        speedl=speedl+400;
+        break;
+     case 's':// backward
+        speedr=speedr-400;
+        speedl=speedl-400;
+        break;
+     case'q':
+        turnLeft();
+        break;
+     case'e':
+        turnRight();
+        break;
+     case'x':
+        speedr = 0;
+        speedl = 0; 
+        break;
+      case'z':
+        turnAround();
+        break;
+     case 'o':
+        mode = 0;
+        break;
+      case'l':
+        doScan();
+        break;
+     case 'r':
+        doScan();
+        break;
+     case 'v':
+        doScan();
+        buzzer.playFrequency(500,250,10);
+        break;
+      }
+      
+      delay(10);
+//  }
+//  }
+}
